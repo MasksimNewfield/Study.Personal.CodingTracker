ec425e598ef5e7b0f26d3dac060828ccf8c9e78f
MaksimNewfield <fxiozil@gmail.com>
2023-08-03 19:24:26 +0200

    Console.WriteLine further modified

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy!");
+
+// Commit for all to make a local save file
+// Push to "upload" it on GitHub
+// Pull to "download" it from GitHub
